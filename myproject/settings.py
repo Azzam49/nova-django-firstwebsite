ba31b7b1e0fa4e7a46c57c0ff62d477d49d6175e
Azzam49 <azzam49coder@gmail.com>
2025-08-06 20:49:05 +0800

    Creating and Registering myapp

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp', # here we register our `myapp` with the project
 ]
 
 MIDDLEWARE = [
