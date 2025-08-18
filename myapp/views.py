3dafc91c32dbfe443e1b076ac2962172e9cd7960
Azzam49 <azzam49coder@gmail.com>
2025-08-18 19:05:16 +0800

    Removing pages

--- myapp/views.py
+++ myapp/views.py
@@ -3,20 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    # HttpReponse :
-        # allows our view function to return HTTP Reponse to the user
-        # HttpResponse : serves as our Template, it return HTML in simple form
-        # HttpResponse cannot return an html file
-    # return HttpResponse('<h1 style="color: red;">Hello World</h1> <h2>Bye</h2>')
-
-    # render :
-        # allows our view function to return HTTP Reponse to the user
-        # But the difference, that render can help us to return an whole HTML file as response
-        # render is used here to return home.html for the user.
-            # home.html is created at : myapp/templates/myapp/home.htm
-    # return render(request, 'home.html', {}) # Error :  TemplateDoesNotExist at /home/ home.html
     return render(request, 'myapp/home.html', {})
-
-
-def my_page(request):
-    return render(request, 'myapp/my-page.html', {})
